3f6a64be1476df0d8a2179ac6538ff0f6883e500
KOFIWILLIAMS <winnerwilliams1@gmail.com>
2023-09-17 22:29:04 -0700

    new

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,6 @@ const getALL = async (req, res) => {
         res.setHeader('Content-Type','application/json');
         res.status(200).json(users);
     });
-
 };
 
 
@@ -18,12 +17,63 @@ const getSingle = async (req, res) => {
         res.setHeader('Content-Type','application/json');
         res.status(200).json(users[0]);
     });
-  
+};
 
+
+const addUser = async (req, res) => {
+
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+      };
+
+      const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+      if (response.acknowledged) {
+        res.status(204).send();
+      }else{
+        res.status(500).json(response.error || 'Error occurred');
+      }
+};
+
+
+const updateUser = async (req, res) => {
+    const userId =new ObjectId(req.params.id);
+    const user = {
+        username: req.body.username,
+        email: req.body.email,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
+      };
+
+      const response = await mongodb.getDatabase().db().collection('users').replaceOne({_id: userId}, user);
+      if (response.modifiedCount > 0) {
+        res.status(204).send();
+      }else{
+        res.status(500).json(response.error || 'Error occurred Updating');
+      }
+};
+
+
+
+const deleteUser = async (req, res) => {
+    const userId =new ObjectId(req.params.id);
+
+      const response = await mongodb.getDatabase().db().collection('users').remove({_id: userId}, true);
+      if (response.deleteCount > 0) {
+        res.status(204).send();
+      }else{
+        res.status(500).json(response.error || 'Error occurred Deleting');
+      }
 };
 
 
 module.exports = {
     getALL,
-    getSingle
+    getSingle,
+    addUser,
+    updateUser,
+    deleteUser
 };
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,13 @@ router.get('/', usersController.getALL);
 
 router.get('/:id', usersController.getSingle);
 
+// Routes are here
+
+router.post('/', usersController.addUser);
+
+router.put('/:id', usersController.updateUser);
+
+router.delete('/:id', usersController.deleteUser);
+
+
 module.exports = router;
